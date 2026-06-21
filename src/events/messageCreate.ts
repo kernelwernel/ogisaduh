07c625d9817ddf4261adc31db65787064e45ab3c
kernelwernel <wpiffting8@gmail.com>
2026-06-21 19:58:34 +0100

    added message log functionality

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -31,6 +31,8 @@ module.exports = {
   async execute(message: Message, client: BotClient) {
     if (message.author.bot) return;
 
+    console.log(`[${new Date().toLocaleTimeString()}] ${message.author.tag}: ${message.content}`);
+
     if (message.author.id === OGISADA_USER_ID && URL_REGEX.test(message.content)) {
       try {
         await message.author.send({ files: [GIF_PATH] });
